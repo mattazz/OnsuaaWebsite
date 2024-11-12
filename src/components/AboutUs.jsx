06d2506d8c94fe6a4283b145eb636afe6fc2b5ae
Mattazz <matthewazada101@gmail.com>
2024-11-12 11:32:11 -0500

    feat: improve About Us component layout for better responsiveness and structure

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,4 @@
-import CircleImage from "./CircleImage";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Footer from "./Footer";
 
 function AboutUs() {
@@ -9,7 +8,7 @@ function AboutUs() {
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
             exit={{ opacity: 0 }}
-            
+
         >
             <div className="flex flex-col justify-center align-middle m-auto text-center my-20 w-1/2 gap-3">
                 <h2 className="text-red-400 font-bold"> Know Everything...About Us</h2>
@@ -22,30 +21,25 @@ function AboutUs() {
                 <h2 className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid fugit sed ipsa voluptatem ipsum vel vitae, expedita nam corrupti vero rerum amet minima dignissimos, veniam nobis aliquam nesciunt. Consequuntur.</h2>
             </div>
 
-            <div className="flex flex-row testBorder p-5 m-5">
-                <div className="flex w-1/2 testBorder justify-center align-middle m-auto">
-                    <h1 className="text-3xl text-center">--IMG HERE--</h1>
+            <div className="flex flex-row flex-wrap  p-5 m-5">
+                <div className="flex w-full sm:w-1/2 lg:w-1/3 justify-center align-middle m-auto ">
+                    <h1 className="text-3xl text-center ">--IMG HERE--</h1>
                 </div>
-                <div className="testBorder w-1/2">
+                <div className="flex w-full sm:w-1/2  lg:w-2/3 ">
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo alias, nihil voluptatem cupiditate, incidunt ipsum quaerat minima accusamus maxime ipsam asperiores eaque architecto et rem, nobis reprehenderit possimus repudiandae distinctio.</p>
                 </div>
             </div>
-            <div className="flex flex-row testBorder p-5 m-5">
-                <div className="testBorder w-1/2">
+            <div className="flex flex-row flex-wrap  p-5 m-5">
+                <div className="flex w-full sm:w-1/2  lg:w-2/3 ">
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo alias, nihil voluptatem cupiditate, incidunt ipsum quaerat minima accusamus maxime ipsam asperiores eaque architecto et rem, nobis reprehenderit possimus repudiandae distinctio.</p>
                 </div>
-                <div className="flex w-1/2 testBorder justify-center align-middle m-auto">
-                    <h1 className="text-3xl text-center">--IMG HERE--</h1>
+                <div className="flex w-full sm:w-1/2 lg:w-1/3 justify-center align-middle m-auto ">
+                    <h1 className="text-3xl text-center ">--IMG HERE--</h1>
                 </div>
             </div>
-
-            
-
-
-            
             <Footer />
         </motion.div>
-        
+
     )
 }
 
