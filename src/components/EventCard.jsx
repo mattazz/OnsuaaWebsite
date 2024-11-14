dfcf466592ec084bebc799c0e7af429aa6cce057
Mattazz <matthewazada101@gmail.com>
2024-11-14 09:22:21 -0500

    feat: enhance EventCard and Events components with framer-motion for animations; update layout and styles for improved user experience

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,16 +1,28 @@
+/* eslint-disable react/prop-types */
+import {motion} from 'framer-motion';
 
-function EventCard(){
+function EventCard({variants}){
+    const spring = {
+        type: "spring",
+        damping: 20,
+        stiffness: 300
+    }
     return(
-        <div className="flex-col flex w-full md:flex-row rounded-md shadow-xl gap-2">
-            <div className="w-full">
+        <motion.div 
+        variants={variants}
+        transition={spring}
+        whileHover={{ scale: 1.05 }}
+        
+        className="flex-col flex w-full sm:p-2 md:flex-row md:w-full md:p-0 lg:p-0 lg:w-full xl:w-[45%] rounded-md shadow-xl gap-2 p-0 ">
+            <div className="w-full md:w-1/2 flex">
                 <img className="eventCardImg rounded-tl-md rounded-bl-md" src="https://www.crmsoftwareblog.com/wp-content/uploads/help-g2d0b94a79_1920.jpg" alt="" />
             </div>
-            <div className="flex flex-col w-full gap-3  pt-3">
-                <h1>Event Title</h1>
+            <div className="flex flex-col w-full md:w-1/2 pt-3 pb-3 pr-3">
+                <h1 className="text-xl font-bold">Event Title</h1>
                 <p className="italic">Event date</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto magni error assumenda exercitationem, beatae minima enim incidunt rem nobis accusantium quod, reiciendis consectetur. </p>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,14 +1,39 @@
 import EventCard from "./EventCard";
+import { motion } from 'framer-motion';
 
 function Events() {
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  }
+
+  const itemVariants = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1 }
+  }
+
+
   return (
-    <div>
+    <>
       <h1>Events</h1>
-      <div className="flex gap-10 p-3 ">
-        <EventCard />
-        <EventCard />
-      </div>
-    </div>
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-wrap w-full gap-10 justify-center p-5 ">
+        <EventCard variants={itemVariants} />
+        <EventCard variants={itemVariants} />
+        <EventCard variants={itemVariants} />
+        <EventCard variants={itemVariants} />
+        <EventCard variants={itemVariants} />
+        <EventCard variants={itemVariants} />
+      </motion.div>
+    </>
   );
 }
 
